de50f9795f5960938a354164906773e2c935147d
Suiginn92409223 <89884762+Suiginn92409223@users.noreply.github.com>
2021-09-13 10:32:16 +0900

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -18,7 +18,7 @@ const app = Vue.createApp({
       */
       correctAnswer: {
         stage1: {
-          q1: 'dcfvg',
+          q1: 'DCFVG',
         },
         stage2: {
           q1: 'いいい',
